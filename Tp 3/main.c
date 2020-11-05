97f3107813440fa5ea92b4fa63c926a5bab13190
Matiassgg <matugrillo@hotmail.com>
2020-11-05 17:04:13 -0300

    Update main.c

--- Tp 3/main.c
+++ Tp 3/main.c
@@ -10,8 +10,6 @@
 */                                                             
 //////////////////////////////////////////////////////////////////////////////////////
 
-
-// Esta cabecera la generas despues de haber hecho "flex scanner.l"
 #include "scanner.h"
 int TOKEN;
 char *token_names[] = {"Fin de archivo","Programa","Declarar","Leer","Escribir","Fin-Prog","Asignacion","Identificador","Constante"};
@@ -30,7 +28,7 @@ int main()
 			printf("Token: %s\tlexema: %s\n",token_names[TOKEN],yytext);
 		}
 		else{
-			// Si es solo un caracter el TOKEN muestro ese caracter
+			// Si es solo un caracter el TOKEN, muestro ese caracter
 			printf("Token: \'%c\'\n",TOKEN);
 		}
 			 
