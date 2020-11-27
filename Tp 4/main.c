868edd951997c263cb7dca383e19aff276d8e239
Matiassgg <matugrillo@hotmail.com>
2020-11-27 19:46:25 -0300

    Agrego mensajes con cada caso para yyparse()

--- Tp 4/main.c
+++ Tp 4/main.c
@@ -5,11 +5,22 @@
 */                                                             
 //////////////////////////////////////////////////////////////////////////////////////
 
-
+#include <stdio.h>
+//#include "parser.h"
+//#include "scanner.h"
 
 int main(void)
 {	
-	
-        
+	switch(yyparse()){
+		case 0:
+			printf("Exito"); printf("Errores sintacticos: X - Errores lexicos: X");	// faltan las variables
+			return 0;
+		case 1:
+			printf("Error"); printf("Errores sintacticos: X - Errores lexicos: X");	// faltan las variables
+			return 1;
+		case 2:
+			printf("Memoria insuficiente");
+			return 2;
+	}
 	return 0;		
 }
